e9d42ad69683cdbaa55dd74449f1c27db1968c90
surrogard <surrogard@gmail.com>
2021-09-23 19:42:09 +0200

    refactoring esp32 cam code

--- CoopCamRev2FTP.ino
+++ CoopCamRev2FTP.ino
@@ -69,169 +69,150 @@ ESP32_FTPClient ftp (ftp_server,ftp_user,ftp_pass, 5000, 2);
 
 void setup()
 {
- pinMode(LED, OUTPUT);
- timer.setInterval(1000,coopCom);
- timer.setInterval(250,loadingImage);
+  pinMode(LED, OUTPUT);
+  timer.setInterval(1000,coopCom);
+  timer.setInterval(250,loadingImage);
   Blynk.setProperty(V0, "url", 1, coopImageURL);
   Blynk.virtualWrite(V0, 1); 
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
   
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
-  config.ledc_timer = LEDC_TIMER_0;
-  config.pin_d0 = Y2_GPIO_NUM;
-  config.pin_d1 = Y3_GPIO_NUM;
-  config.pin_d2 = Y4_GPIO_NUM;
-  config.pin_d3 = Y5_GPIO_NUM;
-  config.pin_d4 = Y6_GPIO_NUM;
-  config.pin_d5 = Y7_GPIO_NUM;
-  config.pin_d6 = Y8_GPIO_NUM;
-  config.pin_d7 = Y9_GPIO_NUM;
-  config.pin_xclk = XCLK_GPIO_NUM;
-  config.pin_pclk = PCLK_GPIO_NUM;
-  config.pin_vsync = VSYNC_GPIO_NUM;
-  config.pin_href = HREF_GPIO_NUM;
+  config.ledc_timer   = LEDC_TIMER_0;
+  config.pin_d0       = Y2_GPIO_NUM;
+  config.pin_d1       = Y3_GPIO_NUM;
+  config.pin_d2       = Y4_GPIO_NUM;
+  config.pin_d3       = Y5_GPIO_NUM;
+  config.pin_d4       = Y6_GPIO_NUM;
+  config.pin_d5       = Y7_GPIO_NUM;
+  config.pin_d6       = Y8_GPIO_NUM;
+  config.pin_d7       = Y9_GPIO_NUM;
+  config.pin_xclk     = XCLK_GPIO_NUM;
+  config.pin_pclk     = PCLK_GPIO_NUM;
+  config.pin_vsync    = VSYNC_GPIO_NUM;
+  config.pin_href     = HREF_GPIO_NUM;
   config.pin_sscb_sda = SIOD_GPIO_NUM;
   config.pin_sscb_scl = SIOC_GPIO_NUM;
-  config.pin_pwdn = PWDN_GPIO_NUM;
-  config.pin_reset = RESET_GPIO_NUM;
+  config.pin_pwdn     = PWDN_GPIO_NUM;
+  config.pin_reset    = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
   config.pixel_format = PIXFORMAT_JPEG; 
   
   if(psramFound()){
-    config.frame_size = FRAMESIZE_SVGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+    config.frame_size   = FRAMESIZE_SVGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
     config.jpeg_quality = 20;
-    config.fb_count = 2;
-  } 
-  else {
-    config.frame_size = FRAMESIZE_VGA;
+    config.fb_count     = 2;
+  } else {
+    config.frame_size   = FRAMESIZE_VGA;
     config.jpeg_quality = 20;
-    config.fb_count = 1;
+    config.fb_count     = 1;
   }
   
   // Init Camera
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
-//    Serial.printf("Camera init failed with error 0x%x", err);
+    // Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
   
   Serial.begin( 115200 );
 
-// WiFi.begin( WIFI_SSID, WIFI_PASS );
+  // WiFi.begin( WIFI_SSID, WIFI_PASS );
   Blynk.begin(auth, WIFI_SSID, WIFI_PASS);
   // Serial.println("Connecting Wifi...");
- // while (WiFi.status() != WL_CONNECTED) {
- //     delay(500);
- //     Serial.print(".");
-//  }
-//  Serial.println("");
-//  Serial.print("IP address: ");
-//  Serial.println(WiFi.localIP());
+  // while (WiFi.status() != WL_CONNECTED) {
+  //   delay(500);
+  //   Serial.print(".");
+  // }
+  // Serial.println("");
+  // Serial.print("IP address: ");
+  // Serial.println(WiFi.localIP());
 }
 
 BLYNK_CONNECTED() {
-    Blynk.syncAll();
+  Blynk.syncAll();
 }
-
-
 
 void loadingImage( void ) {
-      if (loading) {
-      displayImage= "https://coopcommandimages.000webhostapp.com/uploads/";
-      displayImage+= i;
-      displayImage+= ".png";
-      Blynk.setProperty(V0, "url", 1, displayImage); 
-      if (i <= 6) {
-      i ++;
-      }
-      else if (i > 6) {
-        i = 1;
-      }
-      }
-      else if (!loading) {
-        if (ic == 1) {
-        coopImageURL = "https://coopcommandimages.000webhostapp.com/uploads/coopPic2.jpg";
-        }
-        else if (ic == 2) {
-        coopImageURL = "https://coopcommandimages.000webhostapp.com/uploads/coopPic3.jpg";
-        }
-        else if (ic == 3) {
-          coopImageURL = "https://coopcommandimages.000webhostapp.com/uploads/coopPic4.jpg";
-        }
-        Blynk.setProperty(V0, "url", 1, coopImageURL);
-        Blynk.virtualWrite(V0, 1);  
-        i = 1;      
-      }
-
+  if (loading) {
+    displayImage= "https://coopcommandimages.000webhostapp.com/uploads/" + i + ".png";
+    Blynk.setProperty(V0, "url", 1, displayImage); 
+    if (i <= 6) {
+      i++;
+    } else if (i > 6) {
+      i = 1;
+    }
+  } else {
+    if (ic >= 1 && ic <= 3) {
+      coopImageURL = "https://coopcommandimages.000webhostapp.com/uploads/coopPic" + (i+1) + ".jpg";
+    }
+    Blynk.setProperty(V0, "url", 1, coopImageURL);
+    Blynk.virtualWrite(V0, 1);  
+    i = 1;      
+  }
 }
-
  
 void sendPhoto ( void ) {
-   if (takePhoto) {
-
-   camera_fb_t * fb = NULL;
+  if (takePhoto) {
+    camera_fb_t * fb = NULL;
   
-  // Take Picture with Camera
-  fb = esp_camera_fb_get(); 
-  digitalWrite(LED, LOW); 
-  delay (50);
-  if(!fb) {
-//    Serial.println("Camera capture failed");
-    ESP.restart();
-    return;
-  }
+    // Take Picture with Camera
+    fb = esp_camera_fb_get(); 
+    digitalWrite(LED, LOW); 
+    delay (50);
+    if(!fb) {
+      // Serial.println("Camera capture failed");
+      ESP.restart();
+      return;
+    }
 
-if (imageFlip == 1) {
-  ftp.OpenConnection();
-  // Create the new file and send the image
-  ftp.ChangeWorkDir("/public_html/uploads/");
-  ftp.InitFile("Type I");
-  ftp.NewFile("coopPic2.jpg");
-  ftp.WriteData( fb->buf, fb->len );
-  ftp.CloseFile();
-  ftp.CloseConnection();
-  takePhoto = false;
-  imageFlip = 2;
-  ic = 1;
-  esp_camera_fb_return(fb);
-    Serial.print('N');
-   }
-else if (imageFlip == 2) {
-   ftp.OpenConnection();
-  // Create the new file and send the image
-  ftp.ChangeWorkDir("/public_html/uploads/");
-  ftp.InitFile("Type I");
-  ftp.NewFile("coopPic3.jpg");
-  ftp.WriteData( fb->buf, fb->len );
-  ftp.CloseFile();
-  ftp.CloseConnection();
-  takePhoto = false;
-  imageFlip = 3;
-  ic = 2;
-  esp_camera_fb_return(fb);
-  Serial.print('N');
-   }
-else if (imageFlip == 3) {
-   ftp.OpenConnection();
-  // Create the new file and send the image
-  ftp.ChangeWorkDir("/public_html/uploads/");
-  ftp.InitFile("Type I");
-  ftp.NewFile("coopPic4.jpg");
-  ftp.WriteData( fb->buf, fb->len );
-  ftp.CloseFile();
-  ftp.CloseConnection();
-  takePhoto = false;
-  imageFlip = 1;
-  ic = 3;
-  esp_camera_fb_return(fb);
-  Serial.print('N');
-   }
-}
+    if (imageFlip == 1) {
+      ftp.OpenConnection();
+      // Create the new file and send the image
+      ftp.ChangeWorkDir("/public_html/uploads/");
+      ftp.InitFile("Type I");
+      ftp.NewFile("coopPic2.jpg");
+      ftp.WriteData( fb->buf, fb->len );
+      ftp.CloseFile();
+      ftp.CloseConnection();
+      takePhoto = false;
+      imageFlip = 2;
+      ic = 1;
+      esp_camera_fb_return(fb);
+      Serial.print('N');
+    } else if (imageFlip == 2) {
+      ftp.OpenConnection();
+      // Create the new file and send the image
+      ftp.ChangeWorkDir("/public_html/uploads/");
+      ftp.InitFile("Type I");
+      ftp.NewFile("coopPic3.jpg");
+      ftp.WriteData( fb->buf, fb->len );
+      ftp.CloseFile();
+      ftp.CloseConnection();
+      takePhoto = false;
+      imageFlip = 3;
+      ic = 2;
+      esp_camera_fb_return(fb);
+      Serial.print('N');
+    } else if (imageFlip == 3) {
+      ftp.OpenConnection();
+      // Create the new file and send the image
+      ftp.ChangeWorkDir("/public_html/uploads/");
+      ftp.InitFile("Type I");
+      ftp.NewFile("coopPic4.jpg");
+      ftp.WriteData( fb->buf, fb->len );
+      ftp.CloseFile();
+      ftp.CloseConnection();
+      takePhoto = false;
+      imageFlip = 1;
+      ic = 3;
+      esp_camera_fb_return(fb);
+      Serial.print('N');
+    }
+  }
 }
 
 void coopCom ( void ) {
-
   if (Serial.available() > 0) {
     coopRx = Serial.read();
     newDataRx = true;
@@ -270,25 +251,25 @@ void coopCom ( void ) {
 }
 
 BLYNK_WRITE(DOORUP) {
-Serial.print('U');
+  Serial.print('U');
 }
 
 BLYNK_WRITE(DOORDOWN) {
-Serial.print('D');
+  Serial.print('D');
 }
 
 BLYNK_WRITE(V5) {
-digitalWrite(LED, HIGH);
-Serial.print('L'); 
-takePhoto = true;
-loading = true;
-loadingImage();
-timer.setTimeout(250, sendPhoto);
-timer.setTimeout(3000, []()
-{
-  loading = false;
+  digitalWrite(LED, HIGH);
+  Serial.print('L'); 
+  takePhoto = true;
+  loading = true;
   loadingImage();
-});
+  timer.setTimeout(250, sendPhoto);
+  timer.setTimeout(3000, []()
+  {
+    loading = false;
+    loadingImage();
+  });
 }
 
 void loop()
